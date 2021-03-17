7f7ef195bdb41a37100dd3e81e806de665ea1174
Ding Ruiqi <416640656@qq.com>
2021-03-17 23:50:03 +0800

    ..

--- code/success/convertTraj.py
+++ code/success/convertTraj.py
@@ -1,13 +1,23 @@
 
 import os, subprocess
 
-for root, subdirs, files in os.walk('.'):
-  for f in files:
-    if f.endswith('.nc'):
-      filePath = os.path.join(root, f)
-      print(subprocess.run([
-        "mdconvert", 
-        filePath, 
-        "-o",
-        filePath.replace('.nc', '.dcd')
-      ]))
+if 0:
+  for root, subdirs, files in os.walk('.'):
+    for f in files:
+      if f.endswith('.dcd'):
+        filePath = os.path.join(root, f)
+        print(subprocess.run([
+          "mdconvert", 
+          filePath, 
+          "-o",
+          filePath.replace('.dcd', '.h5')
+        ]))
+else:
+  print(subprocess.run([
+    "mdconvert", 
+    r'D:\GitHub\MD\models\ASAP3\step3_pbcsetup.pdb.dcd', 
+    "-o",
+    r'D:\GitHub\MD\models\ASAP3\step3_pbcsetup.xtc',
+    #"-t",
+    #r'D:\GitHub\MD\models\ASAP3\step3_pbcsetup.pdb',
+  ]))
